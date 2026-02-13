ac4ab942960692b4b75d0fdace1ba6723add3f9b
Adoneesk <adonees.kawash@ucalgary.ca>
2026-02-13 12:37:56 -0700

    Question3 Complete

---
+++ question3.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+kidney_disease_csv = pd.read_csv(r"C:\DATA221\Assignments\Assignment3\kidney_disease.csv")
+x = kidney_disease_csv.drop(columns=["classification"])
+y = kidney_disease_csv["classification"]
+
+split = train_test_split(x, y, test_size=0.3, random_state=42)
+x_test = split[0]
+x_train = split[1]
+y_test = split[2]
+y_train = split[3]
+
+"""
+Training and Testing on the Same Model:
+- We should not train and split on the same model
+because the model can accidentally use the training
+examples instead of actually learning general
+patterns. In general, this will lead to overfitting.
+
+Test Set:
+- The purpose of the test set is to evaluate the
+performance of the trained model. Helps direct the
+model to more accurate predictions.
+
+
+"""
+
